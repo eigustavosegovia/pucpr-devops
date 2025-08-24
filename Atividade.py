522bf3745a7ccc9354dea83cd494c7a83d785aba
Gustavo Segovia <gustavosegovia04@gmail.com>
2025-08-24 16:42:15 -0300

    Adicionando arquivo python

---
+++ Atividade.py
@@ -0,0 +1,15 @@
+aluno = {}
+nome = str(input("Digite o nome do aluno: "))
+aluno["Nome"] = nome
+media = float(input("Digite a média do aluno: "))
+aluno["Média"] = media
+if media < 5:
+    aluno["Situação"] = "Reprovado"
+elif media < 7:
+    aluno["Situação"] = "Recuperação"
+else:
+    aluno["Situação"] = "Aprovado"
+print("-=" * 30)
+print(f'''Aluno: {aluno["Nome"]}
+Média: {aluno["Média"]}
+Situação: {aluno["Situação"]}''')
