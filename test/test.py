55f2a379fb53ca891e29a18f2e8376008ee0c15e
Gustavo Segovia <gustavosegovia04@gmail.com>
2025-09-20 15:53:01 -0300

    Atividade Somativa 2

--- test/test.py
+++ test/test.py
@@ -1,6 +1,21 @@
 from src.main import *
 
+def test_somar():
+    assert somar(2, 3) == 5
+    assert somar(-1, 1) == 0
 
-def test_aluno():
-    avaliar_aluno_teste = avaliar_aluno(nome="teste", media= 9)
-    assert avaliar_aluno_teste["Situação"] == "Aprovado"
+def test_subtrair():
+    assert subtrair(10, 4) == 6
+    assert subtrair(0, 5) == -5
+
+def test_multiplicar():
+    assert multiplicar(3, 4) == 12
+    assert multiplicar(-2, 3) == -6
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+    assert dividir(4, 2) == 2
+
+def test_fatorial():
+    assert fatorial(5) == 120
+    assert fatorial(0) == 1
